f15dbd4d019c60d434e9eac9cf85f082cb6d52cf
Dabsia <dabojohnson@gmail.com>
2022-04-12 13:49:04 -0700

    Created state so the user can not submit an empty form

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,25 +1,45 @@
+import React, {useState} from 'react'
 import classes from './Auth.module.css';
 import {useDispatch} from 'react-redux'
 import {authActions} from '../store/auth'
 
 const Auth = () => {
 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errMsg, setErrMsg] = useState(false)
+
+  const getEmail = (e) => {
+    setEmail(e.target.value)
+    
+  }
+
+  const getPassword = (e) => {
+    setPassword(e.target.value)
+    
+  }
+
   const dispatch = useDispatch()
   const loginHandler = (e) => {
     e.preventDefault()
+    if (email === '' && password === ''){
+      setErrMsg(true)
+      return 
+    }
     dispatch(authActions.login())
   }
   return (
     <main className={classes.auth}>
       <section>
+      {errMsg && <p>Please fill in the form to access the app</p>}
         <form onSubmit = {loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <input type='email' id='email' onChange = {getEmail} value = {email}/>
           </div>
           <div className={classes.control}>
             <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <input type='password' id='password' onChange = {getPassword} value = {password}/>
           </div>
           <button>Login</button>
         </form>
